4f228a5d7b42122820613d6381c9c4287af3f324
Justus <justus.vincent.donner@outlook.de>
2022-11-02 17:06:49 +0100

    Implementation of the API interface
    - Create project
    - Create endpoint
    - Test endpoint example
    - Testing the passed parameters

---
+++ DoePaAdminWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace DoePaAdminWebAPI.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("employees")]
+    //[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id:int}/invoiced")]
+        public string Get(int id, string from, string to)
+        {
+            return string.Format("User:{0}\nFrom:{1}\nTo:{2}", id, from, to);
+        }
+    }
+}
